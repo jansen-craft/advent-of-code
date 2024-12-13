7e75ac61422db05773c80a78c732e1c142204aa5
Jansen <jc103820@ohio.edu>
2024-12-12 20:22:31 -0500

    Add day 8

---
+++ 2024/8a.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+using namespace std;
+
+#define ROW first
+#define COL second
+
+vector<string> g;
+set<pair<int,int>> antinodes;
+
+void print_grid(){
+    printf("    ");
+    for (int col = 0; col < g[0].size(); col++){
+        printf("%2i", col);
+    }
+    printf("\n\n");
+    for (int row = 0; row < g.size(); row++){
+        printf("%3i ",row);
+        for (int col = 0; col < g[row].size(); col++){
+            if (antinodes.find({row,col}) != antinodes.end()){
+                printf(" #");
+            } else {
+                printf("%2c", g[row][col]);
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main(){
+    string s;
+    while(getline(cin,s)) g.push_back(s);
+
+    print_grid();
+
+    map<char,set<pair<int,int>>> antennas;
+    for(int row = 0; row < g.size(); row++){
+        for(int col = 0; col < g[row].size(); col++){
+            char c = g[row][col];
+            if (c != '.') antennas[c].insert({row,col});
+        }
+    }
+
+    for(auto c: antennas){
+        cout << c.first << '\n';
+        auto antenna_set = c.second;
+        for (auto a1 = antenna_set.begin(); a1 != antenna_set.end(); ++a1) {
+            for (auto a2 = antenna_set.begin(); a2 != antenna_set.end(); ++a2) {
+                if (a1 == a2) continue;
+
+                int row = (*a1).ROW - ((*a2).ROW -(*a1).ROW);
+                int col = (*a1).COL - ((*a2).COL -(*a1).COL);
+
+                if ((row >= 0 && row < g.size()) && (col >= 0 && col < g[row].size())){
+                    antinodes.insert({row, col});
+                }
+            }
+        }
+    }
+
+    print_grid();
+
+    cout << antinodes.size() << '\n';
+}
