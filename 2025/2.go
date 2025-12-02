afc1e459dde3465d655e30f9901ce345dc22d23c
Jansen <jc103820@ohio.edu>
2025-12-02 08:22:25 -0500

    day 2

--- 2025/2.go
+++ 2025/2.go
@@ -1,9 +1,55 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
+func duplicated(s string) bool {
+	return s[:len(s)/2] == s[len(s)/2:]
+}
+
+func periodic(s string) bool { // repeated substring pattern
+	if len(s) < 2 {
+		return false
+	}
+	ss := (s + s)[1 : len(s)*2-1]
+	return strings.Contains(ss, s) // if ababab in -bababababa- then its periodic
+}
+
 func Day2() {
-	fmt.Printf("hello from day 2\n")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	scanner.Scan()
+	line := scanner.Text()
+	fmt.Println(line)
+
+	sum := 0
+	sum2 := 0
+
+	parts := strings.Split(line, ",")
+
+	for _, part := range parts {
+		nums := strings.SplitN(part, "-", 2)
+		start, _ := strconv.Atoi(nums[0])
+		end, _ := strconv.Atoi(nums[1])
+
+		for ; start <= end; start++ {
+			if duplicated(strconv.Itoa(start)) {
+				fmt.Printf("duplicated: %d\n", start)
+				sum += start
+			}
+
+			if periodic(strconv.Itoa(start)) {
+				fmt.Printf("periodic: %d\n", start)
+				sum2 += start
+			}
+		}
+	}
+
+	fmt.Printf("Sum of duplicated: %d\n", sum)
+	fmt.Printf("Sum of periodic: %d\n", sum2)
 }
